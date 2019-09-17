75b0ce05abf44d289b6161c07dea213956609aa1
Omotoso Grace <Bukola.Omotoso@hcahealthcare.com>
2019-09-17 09:21:28 -0500

    Binary and Sequential Search comparisons

---
+++ SearchAlgorithms/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace SearchAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            string fileName = Environment.CurrentDirectory + @"\log.txt";
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+
+
+                List<int> numbers = new List<int>();
+                List<int> foundSearches = new List<int>();
+                List<int> unfoundSearches = new List<int>();
+                int found = 0;
+                int unfound = 0;
+                int total_n_member = 0;
+                int total_n_not_member = 0;
+                double total_seq_cmp_member = 0;
+                double total_seq_cmp_not_member = 0;
+                double total_bin_cmp_member = 0;
+                double total_bin_cmp_not_member = 0;
+
+
+
+
+                //Run a 100 sample of test cases
+                for (int i = 0; i < 100; i++)
+                {
+                    numbers.Clear();
+
+                    //Generate n numbers as the sample size for each test case 
+                    //n ranges from 1 - 49 where n is an odd number
+                    int n = random.Next(1, 50);
+                    if (n % 2 == 0)
+                        n++;
+                    //Generate n random numbers 
+                    for (int j = 0; j < n; j++)
+                    {
+                        //Numbers range from 1 to n
+                        int val = random.Next(1, n + 1);
+                        numbers.Add(val);
+                    }
+                    //sort the numbers
+                    numbers.Sort();
+                    //Generate a number x between 1 to n and search for it
+                    int x = random.Next(1, n + 1);
+
+                    //Convert the list of numbers to an array of numbers
+                    int[] arr = new int[n];
+                    arr = numbers.ToArray();
+                    sw.WriteLine("Sorted Array");
+                    for (int k = 0; k < arr.Length; k++)
+                    {
+                        sw.Write(arr[k] + " ");
+                    }
+                    sw.WriteLine();
+
+                    SearchArray searchArray = new SearchArray(arr, x);
+                    int bin_comp = searchArray.binarySearchComparisons();
+                    int seq_com = searchArray.SeqSearchComparisons();
+                    sw.WriteLine("Parameters:      \t" + "n\t" + "x\t" + "comparisons");
+
+                    if (numbers.Contains(x))
+                    {
+                        found++;
+                        total_n_member += n;
+                        total_seq_cmp_member += seq_com;
+                        total_bin_cmp_member += bin_comp;
+
+                    }
+                    else
+                    {
+                        unfound++;
+                        total_n_not_member += n;
+                        total_seq_cmp_not_member += seq_com;
+                        total_bin_cmp_not_member += bin_comp;
+                    }
+
+                    sw.WriteLine("Sequetial Search:\t" + n + "\t" + x + "\t" + searchArray.SeqSearchComparisons());
+                    sw.WriteLine("Binary Search:    \t" + n + "\t" + x + "\t" + searchArray.binarySearchComparisons());
+                    sw.WriteLine();
+                }
+                sw.WriteLine();
+                sw.WriteLine("Summary");
+                sw.WriteLine("Total Run:\t" + "100");
+                sw.WriteLine("Found:  \t" + found);
+                sw.WriteLine("UnFound: \t" + unfound);
+                sw.WriteLine();
+                sw.WriteLine("Average Case Analysis of Sequential Search");
+                sw.WriteLine();
+
+                sw.WriteLine("Assumption:      \t" + "n(Avg)" + "\t" + "Avg.Comparison");
+                sw.WriteLine("Member of:       \t" + (total_n_member / found) + " \t" + (total_seq_cmp_member) / found);
+                sw.WriteLine("Not Member of:       \t" + (total_n_not_member / unfound) + " \t" + (total_seq_cmp_not_member) / unfound);
+
+                sw.WriteLine();
+                sw.WriteLine("Average Case Analysis of Binary Search");
+                sw.WriteLine();
+                sw.WriteLine("Assumption:      \t" + "n(Avg)" + "\t" + "Avg.Comparison");
+                sw.WriteLine("Member of:       \t" + (total_n_member / found) + " \t" + total_bin_cmp_member / found);
+                sw.WriteLine("Not Member of:       \t" + (total_n_not_member / unfound) + " \t" + total_bin_cmp_not_member / unfound);
+
+            }
+        }
+}
+}
---
+++ SearchAlgorithms/SearchArray.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchAlgorithms
+{
+    class SearchArray
+    {
+        //Declare class variable which is an array of any data type
+
+        private int[] arr;
+        private int key;
+        public SearchArray(int[] arr, int key)
+        {
+            this.arr = arr;
+            this.key = key;
+        }
+
+        public int binarySearch()
+        {
+            int low = 0;
+            int high = arr.Length;
+            Array.Sort(arr);
+
+            while (low < high)
+            {
+
+                int mid = (high + low) / 2;  //compute the mid index
+
+                if (arr[mid] == key)  //if the mid element matches the key, return the index
+                    return mid;
+
+                //look to the right - upper region if the middle element is less
+                //than the search key
+                else if (arr[mid] < key)
+                    low = mid + 1;
+
+                //the only place the key can be found is the lower region
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+
+
+
+        public int sequentialSearch()
+        {
+            Array.Sort(arr);
+            int len = arr.Length;
+            if (len == 0 || key < arr[0] || key > arr[len - 1])  //array is empty or the element is not found
+                return -1;
+            for (int i = 0; i < len; i++)
+            {
+                if (arr[i] == key)
+                    return i;
+            }
+            return -1;
+        }
+
+        public int SeqSearchComparisons()
+        {
+            int count = 0;
+            int len = arr.Length;
+            if (len == 0)  //array is empty
+                return 0;
+            if (key < arr[0] || key > arr[len - 1]) // if the element does not exist, it should only take one comparison since the array is sorted
+                return 1;
+            for (int i = 0; i < len; i++)
+            {
+                count++;
+                if (arr[i] == key)
+                    return count;
+            }
+            //array will search all the elements if the number to be found is missing and it is in the range 1-n
+            return count;
+        }
+
+        public int binarySearchComparisons()
+        {
+            int count = 0;
+            int low = 0;
+            int high = arr.Length;
+            Array.Sort(arr);
+
+            while (low < high)
+            {
+                count++;
+                int mid = (high + low) / 2;  //compute the mid index
+
+                if (arr[mid] == key)  //if the mid element matches the key, return the index
+                                      //  return mid;
+                    return count;
+
+                //look to the right - upper region if the middle element is less
+                //than the search key
+                else if (arr[mid] < key)
+                    low = mid + 1;
+
+                //the only place the key can be found is the lower region
+                else
+                    high = mid - 1;
+            }
+
+            return count;
+        }
+
+    }
+}
